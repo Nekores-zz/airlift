a90b19aa3fa971b53645ee9de29eabc5f71e3a1f
nekores <sajidxullah@gmail.com>
2021-07-25 03:37:23 +0500

    Installed material ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
     </div>
   );
 }
