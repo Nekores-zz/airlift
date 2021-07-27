41c5ec74666b0521974d1a5eab261be38becaf7d
nekores <sajidxullah@gmail.com>
2021-07-27 10:13:13 +0500

    Added get started route

--- src/containers/slide2/index.jsx
+++ src/containers/slide2/index.jsx
@@ -1,21 +1,8 @@
 import "../../pages/App";
-import { Button, Paragraph, Title } from "../../components";
-import StatusLoader from "../../components/StatusLoader";
+import { Paragraph, Title } from "../../components";
 import { Grid, Container } from "@material-ui/core";
-import { useEffect, useState } from "react";
-import CheckIcon from "@material-ui/icons/Check";
 
 function Slide2() {
-  const [pluginStatus, setPluginStatus] = useState(false);
-  const [text, setText] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => setPluginStatus(true), 3000);
-    if (pluginStatus) {
-      setTimeout(() => setText("Loaded"), 2000);
-    }
-  });
-
   return (
     <div className="content-area">
       <Container>
@@ -25,27 +12,12 @@ function Slide2() {
           justifyContent="space-between"
           alignItems="center"
         >
-          <Grid item sm={4}>
-            <h1> {text}</h1>
-            <Title color="purple">Install AirLift to get coins</Title>
+          <Grid item>
+            <Title color="purple">Get started</Title>
             <Paragraph>
               In order to reserve your username and collect coin you have to
               install AirLift plugin.
             </Paragraph>
-            {pluginStatus ? (
-              <StatusLoader
-                statusText="Plugin Installed...."
-                icon={<CheckIcon />}
-              />
-            ) : (
-              <>
-                <StatusLoader statusText="Checking for plugin...." />
-                <Button> Download</Button>
-              </>
-            )}
-          </Grid>
-          <Grid item>
-            <h1> image herew</h1>
           </Grid>
         </Grid>
       </Container>
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Slide1 from "../containers/slide1";
+import Slide2 from "../containers/slide2";
 import App from "./App";
 
 function Main() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={App} />
-        {/* <Route path="/about" component={About} /> */}
+        <Route exact path="/airlift" component={App} />
+        <Route path="/get-started" component={Slide2} />
         {/* <Route path="/service" component={Service} /> */}
       </Switch>
     </Router>
