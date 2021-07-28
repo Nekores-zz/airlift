06c6002c48b97b0fccf1c0244393687a76ca2005
nekores <sajidxullah@gmail.com>
2021-07-28 06:15:25 +0500

    Work Update on futher screens

--- src/containers/slide2/index.jsx
+++ src/containers/slide2/index.jsx
@@ -1,28 +1,37 @@
 import "../../pages/App";
 import { Paragraph, Title } from "../../components";
-import { Grid, Container } from "@material-ui/core";
+import { Grid, Container, withStyles } from "@material-ui/core";
+import { styleSheet } from "../../jss/pages/slide2";
 
-function Slide2() {
+function Slide2(props) {
+  const { classes } = props;
   return (
-    <div className="content-area">
-      <Container>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Grid item>
-            <Title color="purple">Get started</Title>
-            <Paragraph>
-              In order to reserve your username and collect coin you have to
-              install AirLift plugin.
-            </Paragraph>
+    <div className={classes.root}>
+      <div className="overlay">
+        <Container>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Title color="purple">
+                Lovely to have you with us! <br />
+                Make yourself comfortable.
+                <br />
+                Put on some headphones and
+                <br />
+                Set them at a pleasant volume.
+                <br />
+                Sit back, relax, nand enjoy.
+              </Title>
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </div>
     </div>
   );
 }
 
-export default Slide2;
+export default withStyles(styleSheet, { name: "Slide2" })(Slide2);
---
+++ src/containers/slide3/index.jsx
@@ -0,0 +1,5 @@
+function Slide3() {
+  return <div>Slide 3</div>;
+}
+
+export default Slide3;
